9843e8a69fbf88513277945d9303d9b3369e855b
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-05-14 13:49:15 +0600

    logout fixed

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ async function run() {
 		app.post("/logout", async (req, res) => {
 			const user = req.body
 			console.log("logging out", user)
-			res.clearCookie("token", { maxAge: 0 }).send({ success: true })
+			res.clearCookie("token", { ...cookieOptions, maxAge: 0 }).send({ success: true })
 		})
 
 		app.get("/assignment", logger, async (req, res) => {
