d4d5b8021e15effdd9dcbde2eac1e2b582ea2036
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-05-14 13:04:56 +0600

    token added

--- index.js
+++ index.js
@@ -69,19 +69,19 @@ async function run() {
 
 		// auth related api
 		//creating Token
-		// app.post("/jwt", logger, async (req, res) => {
-		// 	const user = req.body
-		// 	console.log("user for token", user)
-		// 	const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-
-		// 	res.cookie("token", token, cookieOptions).send({ success: true })
-		// })
-
-		app.post("/logout", async (req, res) => {
+		app.post("/jwt", logger, async (req, res) => {
 			const user = req.body
-			console.log("logging out", user)
-			res.clearCookie("token", { maxAge: 0 }).send({ success: true })
+			console.log("user for token", user)
+			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+
+			res.cookie("token", token, cookieOptions).send({ success: true })
 		})
+
+		// app.post("/logout", async (req, res) => {
+		// 	const user = req.body
+		// 	console.log("logging out", user)
+		// 	res.clearCookie("token", { maxAge: 0 }).send({ success: true })
+		// })
 
 		app.get("/assignment", logger, async (req, res) => {
 			const cursor = assignmentCollection.find()
